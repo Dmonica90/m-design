a59bd2f3d34a1e4e066fc52473aa44c36e81f40c
Dmonica90 <dmonica.vrl@gmail.com>
2026-04-02 19:56:24 -0600

    names-challenge

--- src/pages/PlaygroundPage.jsx
+++ src/pages/PlaygroundPage.jsx
@@ -11,37 +11,37 @@ const PlaygroundPage = () => {
             titleKey: "challenge1Title",
             descKey: "challenge1Desc",
             thumbnail: "/thumbnails/dopamine.png",
-            demoLink: "/challenges/operation-dopamine/story.html" // Ruta larga y segura
+            demoLink: "/challenge/operation-dopamine/story.html" // Ruta larga y segura
         },
         {
             titleKey: "challenge2Title",
             descKey: "challenge2Desc",
             thumbnail: "/thumbnails/Squad.png",
-            demoLink: "/challenges/meet-the-squad/story.html" // Ruta larga y segura
+            demoLink: "/challenge/meet-the-squad/story.html" // Ruta larga y segura
         },
         {
             titleKey: "challenge3Title",
             descKey: "challenge3Desc",
             thumbnail: "/thumbnails/Survive.png",
-            demoLink: "/challenges/survive-stakeholder/index.html" // O story.html según tu carpeta original
+            demoLink: "/challenge/survive-stakeholder/index.html" // O story.html según tu carpeta original
         },
         {
             titleKey: "challenge4Title",
             descKey: "challenge4Desc",
             thumbnail: "/thumbnails/slider-accordion.png",
-            demoLink: "/challenges/sliding-accordion/story.html" // Ruta larga y segura
+            demoLink: "/challenge/sliding-accordion/story.html" // Ruta larga y segura
         },
         {
             titleKey: "challenge5Title",
             descKey: "challenge5Desc",
             thumbnail: "/thumbnails/8m-museo.png",
-            demoLink: "/challenges/museo-8m/index.html" // Ruta larga y segura
+            demoLink: "/challenge/museo-8m/index.html" // Ruta larga y segura
         },
         {
             titleKey: "challenge6Title",
             descKey: "challenge6Desc",
             thumbnail: "/thumbnails/mangrove-blur.png",
-            demoLink: "/challenges/mangrove-blur/story.html" // Ruta larga y segura
+            demoLink: "/challenge/mangrove-blur/story.html" // Ruta larga y segura
         }
     ];
     return (
