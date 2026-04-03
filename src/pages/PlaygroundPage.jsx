a7136f4dbae36040c7af73030dbd487c489257a4
Dmonica90 <dmonica.vrl@gmail.com>
2026-04-02 18:44:41 -0600

    mangrove_1

--- src/pages/PlaygroundPage.jsx
+++ src/pages/PlaygroundPage.jsx
@@ -41,7 +41,7 @@ const PlaygroundPage = () => {
             titleKey: "challenge6Title",
             descKey: "challenge6Desc",
             thumbnail: "/thumbnails/mangrove-blur.png",
-            demoLink: "/challenges/mangrove-blur/story.html"
+            demoLink: "/challenges/Mangrove-blur/story.html"
         }
     ];
     return (
